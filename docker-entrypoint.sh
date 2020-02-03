99ae13aaec43f87ea7a062615449bf77115ac3be
Meng Li <meng.li@linecorp.com>
2020-02-03 17:57:40 +0900

    Fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sed -i 's/127.0.0.1:3306/'$DB_SERVER':'$DB_PORT'/g' /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.properties
 sed -i 's/rate/'$DB_NAME'/g' /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.properties
-sed -i 's/username=www/db.user='$DB_USER'/g' /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.properties
-sed -i 's/password=/db.password='$DB_PASSWORD'/g' /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.properties
+sed -i 's/username=www/username='$DB_USER'/g' /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.properties
+sed -i 's/password=/password='$DB_PASSWORD'/g' /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.properties
 sh /usr/local/tomcat/bin/catalina.sh run
